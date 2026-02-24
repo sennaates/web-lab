9123434f22e7e85cd88f8efa9d312d38e647af84
Sena Ateş <147206070+Sena-ates@users.noreply.github.com>
2026-02-24 20:53:31 +0300

    İlk commit: Proje dosyaları eklendi

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 >Web Tasarimi ve Programlama </h1 >
+      <h2 >LAB -1 </h2 >
+      <p>Ad Soyad : [ Kendi Adin ]</p>
+      <p> Ogrenci No: [ Numaran ]</p>
+    </div >
+  )
+}
+
+export default App
